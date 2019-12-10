eb9e4d96f0564eb37c3542f657143d235e964062
carlaraya <carl.araya256@gmail.com>
2019-12-10 10:13:27 +0800

    commit past stuff

--- routes/files.js
+++ routes/files.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('files/index');
+});
+
+router.get('/upload', function(req, res, next) {
+  res.render('files/upload');
 });
 
 module.exports = router;
